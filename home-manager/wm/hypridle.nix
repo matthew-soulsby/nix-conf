4028b730fba3f320b7c2cd26cabea9ee6df5210f
matthew-soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2024-12-10 18:30:06 +1100

    Fix lock

--- home-manager/wm/hypridle.nix
+++ home-manager/wm/hypridle.nix
@@ -4,16 +4,24 @@
   services.hypridle = {
     enable = true;
     settings = {
+      "$lock" = "pidof hyprlock || hyprlock";
+
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
+        lock_cmd = "$lock";
+        before_sleep_cmd = "$lock";
         after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
       };
 
       listener = [
         {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "$lock";
         }
         {
           timeout = 380;
