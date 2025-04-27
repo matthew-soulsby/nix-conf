ced8bd710268e1b07fbb9b7fe6015734f927802b
matthew-soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-04-27 17:44:20 +1000

    Add better copy paste binds

--- home-manager/term/kitty.nix
+++ home-manager/term/kitty.nix
@@ -6,6 +6,12 @@
     shellIntegration = {
       enableZshIntegration = true;
     };
+
+    keybindings = {
+      "ctrl+c" = "copy_or_interrupt";
+      "ctrl+v" = "paste_from_clipboard";
+    };
+
     settings = {
       # Disable close confirmation
       confirm_os_window_close = 0;
