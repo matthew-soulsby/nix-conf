2c332af0b4f123887db841bdaf38be545fbbed03
matthew-soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-08-28 18:17:00 +1000

    Add cursor trail

--- home-manager/term/kitty.nix
+++ home-manager/term/kitty.nix
@@ -15,6 +15,10 @@
     settings = {
       # Disable close confirmation
       confirm_os_window_close = 0;
+
+      # Smooth cursor
+      cursor_trail = "3";
+      cursor_trail_decay = "0.1 0.4";
     };
   };
 }
