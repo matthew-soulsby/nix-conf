2eb0c4ab42ba037e85771bb6414c5b2897f27619
matthew-soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-09-05 17:20:19 +1000

    Remove redundant comments

--- home-manager/wm/scripts/fullscreen-gaps.sh
+++ home-manager/wm/scripts/fullscreen-gaps.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 
-# these two are your gap sizes
 DEFAULT_GAP=10
 MAX_GAP=5
 
-# where Hyprland publishes events
 SOCKET="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
 
-# function to react to events
 handle_event() {
-    # Get the fullscreen state of the currently active window
     local is_fullscreen
     is_fullscreen=$(hyprctl activeworkspace -j | jq -r ".hasfullscreen")
 
     if [[ "$is_fullscreen" =~ "true" ]]; then
-        # The focused window is fullscreen
         hyprctl keyword general:gaps_out $MAX_GAP
     else
-        # The focused window is not fullscreen
         hyprctl keyword general:gaps_out $DEFAULT_GAP
     fi
 }
 
-# subscribe to the socket for activewindowv2 and dispatch to handler
+# subscribe to the socket, and update gap size if relevant event is dispatched
 socat - UNIX-CONNECT:"$SOCKET" | while read -r ev; do
     case $ev in
     "activewindowv2>>"*)
