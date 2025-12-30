ce1a4262fab715702da479a5311cec1e95ff52c0
matthew-soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-12-30 14:59:23 +1100

    Add cups

--- nixos/printing.nix
+++ nixos/printing.nix
@@ -1,7 +1,13 @@
-{...}:
+{pkgs, ...}:
 
 {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+    };
     services.avahi = {
       enable = true;
       nssmdns4 = true;
