7eefccfa26cea59c8bf14f9149c5473c940c355e
Matthew Soulsby <m.soulsby@easygo.io>
2026-01-06 09:32:57 +1100

    Add typo stuff

--- home-manager/editor/nvim/config/init.lua
+++ home-manager/editor/nvim/config/init.lua
@@ -1,4 +1,5 @@
 require("vim-options")
+require("vim-keybinds")
 require("config.treesitter")
 require("config.lazy")
 
---
+++ home-manager/editor/nvim/config/lua/vim-keybinds.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_create_user_command('W', 'w', { nargs = 0 })
+vim.api.nvim_create_user_command('Wq', 'wq', { nargs = 0 })
+vim.api.nvim_create_user_command('Q', 'q', { nargs = 0 })
